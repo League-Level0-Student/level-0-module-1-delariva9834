ea22a42289a9bfa35acdb40528dd4d63fad61de5
Syu125 <yusophia2011@gmail.com>
2017-11-15 02:24:24 -0800

    UnBirthday TheRiddler

---
+++ src/UnBirthday.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class UnBirthday {
+
+	public static void main(String[] args) {
+		String Today= "11/14/17";
+		String Birthday =JOptionPane.showInputDialog( "when is your birthday");
+		if(Today.equals(Birthday)){
+		JOptionPane.showMessageDialog(null, "Happy Birthday");
+		}else{JOptionPane.showMessageDialog(null, "Happy UnBirthday");
+		JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+		}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
