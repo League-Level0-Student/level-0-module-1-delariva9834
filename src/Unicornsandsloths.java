9c9985e4c8d5225076cbd3fb09c7631b246aa44b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-12 19:21:29 -0800

    Unicornandsloths.java-src

---
+++ src/Unicornsandsloths.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane;
+
+public class Unicornsandsloths {
+
+	
+	
+	
+	
+
+
+public static void main(String[] args) {
+	
+	
+
+String Animal= JOptionPane.showInputDialog(" what is your favorite animal?");
+	
+	
+	if(Animal.equals( "Unicorn")) {
+	JOptionPane.showMessageDialog(null, "Unicorns are awsome");
+	
+
+	}
+	
+	else if(Animal.equals ("sloths")){
+
+	JOptionPane.showMessageDialog(null, "Sloths are slow") ;
+	
+	}
+	else if(Animal.equals ("kittens")){
+
+		JOptionPane.showMessageDialog(null, "kittens are ugly and usless") ;
+
+
+}
+}
+}
+	
+
+
