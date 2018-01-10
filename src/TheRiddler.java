d1940656e01001ac2f7422726d90cf8afaf387fd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-09 19:20:37 -0800

    TheRiddler UnBirthday

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,24 +1,34 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-int score=0;
+		int score = 0;
+
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String Variable = JOptionPane.showInputDialog(
+				"If you have me, you want to share me. If you share me, you haven't got me. What am I?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (Variable.equals("nothing")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "correct");
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+			// 5. Otherwise, say "wrong" and tell them the answer
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+			JOptionPane.showMessageDialog(null, "nothing");
+			// 6. Add some more riddles
 
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
-		
+			// 2. Make a pop up to show the score.
+			
+			
+		}
+		JOptionPane.showMessageDialog(null, score);
 	}
 }
-
--- src/UnBirthday.java
+++ src/UnBirthday.java
@@ -3,21 +3,14 @@ import javax.swing.JOptionPane;
 public class UnBirthday {
 
 	public static void main(String[] args) {
-		String Today= "11/14/17";
-		String Birthday =JOptionPane.showInputDialog( "when is your birthday");
-		if(Today.equals(Birthday)){
-		JOptionPane.showMessageDialog(null, "Happy Birthday");
-		}else{JOptionPane.showMessageDialog(null, "Happy UnBirthday");
-		JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+		String Today = "11/14/17";
+		String Birthday = JOptionPane.showInputDialog("when is your birthday");
+		if (Today.equals(Birthday)) {
+			JOptionPane.showMessageDialog(null, "Happy Birthday");
+		} else {
+			JOptionPane.showMessageDialog(null, "Happy UnBirthday");
+			JOptionPane.showMessageDialog(null, "Happy Unbirthday");
 		}
 
-	
-	
-	
-	
-	
-	
-	
-	
 	}
 }
